9f19eb5f36c393e882af76e32cd8100bc2cc863a
JohniCassere <58058388+johnicassere@users.noreply.github.com>
2022-08-29 13:22:11 -0300

    add login

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user.controller');
 //const { validId, validObjectBody } = require('../middlewares/user.middlewares');
-const authMiddleware = require("../middlewares/auth.middleware");
+//const authMiddleware = require("../middlewares/auth.middleware");
 
 
 router.post('/create',  userController.createUserController);
 router.get('/all-users', userController.findUsersController);
-router.get('/one-user/:id', authMiddleware, userController.findUserByIdController);
-router.put('/update-user/:id', authMiddleware, userController.updateUserController);
-router.delete('/delete/:id', authMiddleware, userController.deleteUserController);
+router.get('/one-user/:id', userController.findUserByIdController);
+router.put('/update-user/:id', userController.updateUserController);
+router.delete('/delete/:id', userController.deleteUserController);
 
 
 
